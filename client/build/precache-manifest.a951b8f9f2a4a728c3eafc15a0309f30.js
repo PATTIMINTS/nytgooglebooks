c55e2df2cb1ac64adc96c130083522ab768c2378
Pat Seade <pattimint6@gmail.com>
2019-01-07 17:12:20 -0600

    starting app

---
+++ client/build/precache-manifest.a951b8f9f2a4a728c3eafc15a0309f30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "927b8a53f956d3910cb9",
+    "url": "/static/css/main.5e379b15.chunk.css"
+  },
+  {
+    "revision": "927b8a53f956d3910cb9",
+    "url": "/static/js/main.927b8a53.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  },
+  {
+    "revision": "1cebecb90dfbbf8e9e80a11fef1674d4",
+    "url": "/index.html"
+  }
+];
