ff4aa2e543e56c97f4594ff8681afb4b5a3aca17
Pat Seade <pattimint6@gmail.com>
2019-01-11 22:32:12 -0600

    routes

--- client/src/components/BookItem/index.js
+++ client/src/components/BookItem/index.js
@@ -12,9 +12,9 @@ const BookItem = ({ book, onBookSelect }) => {
             <img className="ui image" src={book.volumeInfo.imageLinks.thumbnail} alt="" />
             <div className="content">
             <div className="ui buttons">
-                <a onClick={() => onBookSelect(book)} className="ui button" href={"/books/Detail"}id="btn">View</a>
+                <a onClick={() => onBookSelect(book)} className="ui button" href={"/books/:id"}id="btn">View</a>
                 <div className="or"></div>
-                <a className="ui brown button" href={"./books/:id"}>Save</a>
+                <a className="ui brown button" href={"/books/:id"}>Save</a>
             </div>
                 <h2 className="header">{book.volumeInfo.title}</h2>
                 <h4>{book.volumeInfo.authors}</h4>
